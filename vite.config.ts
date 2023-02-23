59200ff26be4065a2bf7a43d27ea278bd6311753
tigger <928489257@qq.com>
2023-02-23 10:21:59 +0800

    add dependency auto-import-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+// Auto import APIs on-demand for Vite
+import AutoImport from 'unplugin-auto-import/vite';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      /* options */
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
